53206cb36a03febd2fd25f3b2c9d61e271701583
ouhouh1936 <ouhouh1936@gmail.com>
2021-11-06 15:45:00 +0900

    11/06

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const checkLogion = require("../middlewares/checkLogin");
 const router = express.Router();
+const db = require("../db");
 
 router.get("/", checkLogion, (req, res, next) => {
   const loggedIn = req.session.isLoggedIn;
@@ -10,6 +11,40 @@ router.get("/", checkLogion, (req, res, next) => {
 router.get("/signup", checkLogion, (req, res, next) => {
   const loggedIn = req.session.isLoggedIn;
   res.render("screens/signup", { loggedIn });
+});
+router.post("/signup", (req, res, next) => {
+  const insertQuery = `
+    INSERT INTO users (
+      userId,
+      password,
+      name,
+      mobile,
+      createdAt,
+      updatedAt
+    ) VALUES (
+      "${req.body.input_email}",
+      "${req.body.input_password}",
+      "${req.body.input_name}",
+      "${req.body.input_mobile}",
+      now(),
+      now()
+    )
+  `;
+
+  try {
+    db.query(insertQuery, (err, rows) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      if (!err) {
+        return res.redirect("/");
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send("회원가입에 실패했습니다.");
+  }
 });
 router.get("/signin", checkLogion, (req, res, next) => {
   const loggedIn = req.session.isLoggedIn;
